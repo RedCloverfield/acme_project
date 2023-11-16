e24b1d1992be4b3ccf52ab6884bb204b25bff9c7
Станислав Ефимов <stanislav.karov@mail.ru>
2023-11-16 22:09:16 +0300

    Third commit

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -17,6 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
+    # подключаем библиотеку Django Bootstrap
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
